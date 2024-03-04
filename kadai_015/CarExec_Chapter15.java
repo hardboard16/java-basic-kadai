850e29a0f4b57b0c9b29c51bc8922c7a36e97648
hardboard16 <155826550+hardboard16@users.noreply.github.com>
2024-03-05 05:05:56 +0900

    kadai_015を追加

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		Car_Chapter15 changer = new Car_Chapter15(0, 0);
+		changer.gearChange(3);
+		changer.run();
+
+	}
+
+}
+
+	
+
+
